7fb843ec364aa8587cd4675cd340b6970f13dff9
vkex <82879533+vkex@users.noreply.github.com>
2021-04-21 16:11:11 +0500

    Add files via upload

---
+++ precache-manifest.7878f0b46e2c9891face98a1e8f3fcde.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30c691dc39d9a6accdba5674f1d5ccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7251161bec8c3aff778",
+    "url": "/static/css/2.fb75dcb6.chunk.css"
+  },
+  {
+    "revision": "9df28a0f08ad9dcfe9c0",
+    "url": "/static/css/main.85dc42b3.chunk.css"
+  },
+  {
+    "revision": "c7251161bec8c3aff778",
+    "url": "/static/js/2.9094820b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9094820b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df28a0f08ad9dcfe9c0",
+    "url": "/static/js/main.bcd0e10a.chunk.js"
+  },
+  {
+    "revision": "8114d0eab9e0b62b6691",
+    "url": "/static/js/runtime-main.bbd9d7f3.js"
+  },
+  {
+    "revision": "2f536577cd5da2bef947470d2779085d",
+    "url": "/static/media/icon_vkcoin.2f536577.svg"
+  },
+  {
+    "revision": "091841d07929a9f1a8ca7f3ee76f27f4",
+    "url": "/static/media/icon_vkcoin_dark.091841d0.svg"
+  }
+]);
